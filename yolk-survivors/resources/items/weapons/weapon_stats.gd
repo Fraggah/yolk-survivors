2d305b7cc8afa7662c382dc5108a2c6e64020a97
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2026-01-08 16:17:19 -0300

    Weapon System, stats

--- yolk-survivors/resources/items/weapons/weapon_stats.gd
+++ yolk-survivors/resources/items/weapons/weapon_stats.gd
@@ -9,3 +9,9 @@ class_name WeaponStats
 @export var max_range := 150.0
 @export var knockback := .0
 @export_range(.0, 1.0) var life_steal := .0
+@export var recoil := 25.0
+@export_range(.1, 3.0) var recoil_duration := .1
+@export_range(.1, 3.0) var attack_duration := .2
+@export_range(.1, 3.0) var back_duration := .15
+@export var projectile_scene: PackedScene
+@export var projectile_speed := 1500.0
