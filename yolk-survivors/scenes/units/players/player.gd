38787cac15b9ac0383586bf4c0194a5586c49a91
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2026-01-11 18:29:12 -0300

    Custom enemies behavior

--- yolk-survivors/scenes/units/players/player.gd
+++ yolk-survivors/scenes/units/players/player.gd
@@ -27,11 +27,11 @@ func _ready() -> void:
 	#add_weapon(preload("res://resources/items/weapons/melee/mace/item_mace_1.tres"))
 	#add_weapon(preload("res://resources/items/weapons/melee/sword/item_sword_1.tres"))
 	#add_weapon(preload("res://resources/items/weapons/melee/chainsaw/item_chainsaw_1.tres"))
-	add_weapon(preload("res://resources/items/weapons/range/pistol/item_pistol_1.tres"))
-	add_weapon(preload("res://resources/items/weapons/range/laser/item_laser_1.tres"))
-	add_weapon(preload("res://resources/items/weapons/range/smg/item_smg_1.tres"))
-	add_weapon(preload("res://resources/items/weapons/range/shotgun/item_shotgun_1.tres"))
-	add_weapon(preload("res://resources/items/weapons/range/revolver/item_revolver_1.tres"))
+	#add_weapon(preload("res://resources/items/weapons/range/pistol/item_pistol_1.tres"))
+	#add_weapon(preload("res://resources/items/weapons/range/laser/item_laser_1.tres"))
+	#add_weapon(preload("res://resources/items/weapons/range/smg/item_smg_1.tres"))
+	#add_weapon(preload("res://resources/items/weapons/range/shotgun/item_shotgun_1.tres"))
+	#add_weapon(preload("res://resources/items/weapons/range/revolver/item_revolver_1.tres"))
 
 func _process(delta: float) -> void:
 	move_dir = Input.get_vector("move_left","move_right","move_up","move_down")
