6048ddab43326b8d0a5a195b78973a78188b0357
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2026-01-12 17:23:29 -0300

    Upgrade panel

--- yolk-survivors/scenes/ui/upgrades/upgrade_card.gd
+++ yolk-survivors/scenes/ui/upgrades/upgrade_card.gd
@@ -1,2 +1,6 @@
 extends Panel
 class_name UpgradeCard
+
+@onready var item_icon: TextureRect = %ItemIcon
+@onready var item_name: Label = %ItemName
+@onready var item_description: Label = %ItemDescription
